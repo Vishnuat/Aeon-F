69cec4cdf278c94d741d3d7146f1c8acf092f8a5
Vishnuat <120106924+Vishnuat@users.noreply.github.com>
2024-12-29 18:42:51 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -6,7 +6,7 @@ class _BotCommands:
         self.StartCommand = "start"
         self.MirrorCommand = [f"mirror{i}", f"m{i}"]
         self.YtdlCommand = [f"ytdl{i}", f"y{i}"]
-        self.LeechCommand = [f"leech{i}", f"l{i}"]
+        self.LeechCommand = [f"leech{i}", f"l{i}", "rssl"]
         self.YtdlLeechCommand = [f"ytdlleech{i}", f"yl{i}"]
         self.CloneCommand = [f"clone{i}", f"c{i}"]
         self.CountCommand = f"count{i}"
@@ -16,24 +16,24 @@ class _BotCommands:
         self.SearchCommand = f"search{i}"
         self.StatusCommand = [f"status{i}", "statusall"]
         self.UsersCommand = f"users{i}"
-        self.AuthorizeCommand = f"authorize{i}"
-        self.UnAuthorizeCommand = f"unauthorize{i}"
+        self.AuthorizeCommand = [f"authorize{i}", f"a{i}"]
+        self.UnAuthorizeCommand = [f"unauthorize{i}", f"ua{i}"]
         self.AddSudoCommand = f"addsudo{i}"
         self.RmSudoCommand = f"rmsudo{i}"
-        self.PingCommand = "ping"
-        self.RestartCommand = [f"restart{i}", "restartall"]
-        self.StatsCommand = [f"stats{i}", "statsall"]
+        self.PingCommand = ["ping", f"p{i}"]
+        self.RestartCommand = [f"restart{i}", f"r{i}", "restartall"]
+        self.StatsCommand = [f"stats{i}", f"s{i}", "statsall"]
         self.HelpCommand = f"help{i}"
         self.LogCommand = f"log{i}"
         self.ShellCommand = f"shell{i}"
         self.EvalCommand = f"eval{i}"
         self.ExecCommand = f"exec{i}"
-        self.BotSetCommand = f"botsettings{i}"
-        self.UserSetCommand = f"settings{i}"
+        self.BotSetCommand = [f"botsettings{i}", f"bs{i}"]
+        self.UserSetCommand = [f"usetting{i}", f"us{i}"]
         self.SpeedCommand = f"speedtest{i}"
         self.AddImageCommand = f"addimg{i}"
         self.ImagesCommand = f"images{i}"
-        self.MediaInfoCommand = f"mediainfo{i}"
+        self.MediaInfoCommand = [f"mediainfo{i}", f"mi{i}"]
         self.BroadcastCommand = [f"broadcast{i}", "broadcastall"]
 
 
